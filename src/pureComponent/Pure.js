b103e9d276a83ee05e4de602e25edcecda253cc1
teesha2201 <131008053+teesha2201@users.noreply.github.com>
2023-09-14 13:22:39 +0530

    Pure.js

--- src/pureComponent/Pure.js
+++ src/pureComponent/Pure.js
@@ -24,7 +24,9 @@ class Pure extends  Component
                 <User name={this.state.name} age={this.state.age} eligible={this.state.eligible}/>
                    
                 <button onClick={()=>this.setState({name:"Teesha", age:"22" , eligible:"Yes"})}>Validated</button>
-             
+                 <br></br>
+                <p>In this example  first using state variable declaring keys i.e name, age,eligible of object and initialized as blank string  and passing from component and accepting using onClick event inside another Component in form of props so first time it will render because it will update from blank string to some input but after these it will not re-render whenever we click  on button validated because we are again try to update with same input . in such way it shows the example of PureComponent. </p>
+
 
                 
             </div>
