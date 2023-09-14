3025e8ee85a450bc992a5a9250b31367dc89649c
teesha2201 <131008053+teesha2201@users.noreply.github.com>
2023-09-14 13:23:39 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import Pure from "./pureComponent/Pure"
 function App() {
   return (
     <div className="App">
-          <h2>PureComponent using below Example</h2>
+         
+          <p style={{fontSize:"20px"}}><h3>Pure Component's Defintion:</h3>The Pure Component are the component that do not re-render when props and state has been updated with the same values,so it will increase the performance of your application and website.</p>
+          <h4>PureComponent using below Example</h4>
          <Pure/>
+         <p style={{fontSize:"20px"}}><h3>High Order Component's (HOC) Defintion:</h3>The high order component is an advanced technique  in react for re-using Component logic across Multiple Components .The Hoc is a function that takes  a Component as an Argument and return a new component that wraps the original Component. </p>
          <h2>Higher Order Component using below Example</h2>
-        < Inherit compopass={HighOrderCompo}/>
+          < Inherit compopass={HighOrderCompo}/>
+          <p>In above Hoc Exapmle we are getting product of 12  with natural no. using  clicking on  button inside all that Hoc component seperately with using differnt styling in all those Component . we are passing as a Component as args inside other Component and accepting  as  props and using that Component three times with differnt stlying. In such Manner it will reflect as example of Hoc.</p>
+
     </div>
   );
 }
